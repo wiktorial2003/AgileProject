fde9e870c9f7c818d87bf71c18bd4d7584eb86ac
A00313256 <A00313256@AT-V105-PC10.tusstudent.ad.tus.ie>
2024-10-08 11:24:03 +0100

    added base project folder with Customer and Publication classes

--- Newspaper_Service/src/Publication.java
+++ Newspaper_Service/src/Publication.java
@@ -1,6 +1,14 @@
 
 public class Publication {
 
+	private String name;
+	private String type;
+	private String date;
+	private double price;
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
