9e9d42f8c126916b62ab4338bbdb16a0a3ea2670
A00311282 <A00311282@169.254.69.246>
2024-10-14 15:07:25 +0100

    Resolved errors and made some chnages to DeliveryDocket class, created skeleton for DocketHandler and DocketTest class.

--- Newspaper_Service/src/Docket/DocketHandler.java
+++ Newspaper_Service/src/Docket/DocketHandler.java
@@ -1,5 +1,15 @@
 package Docket;
 
 public class DocketHandler extends Exception {
+	
+	String message;
+	
+	public DocketHandler(String errMessage) {
+		message  = errMessage;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
 
 }
